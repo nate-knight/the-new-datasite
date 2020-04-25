11eec4e09aeee72d20f9eed155fd4a9e18e430ab
nate knight <nateknight@gmail.com>
2020-04-25 00:25:49 -0500

    diligence routing

--- libs/diligence/feature-shell/src/lib/diligence-feature-shell.module.ts
+++ libs/diligence/feature-shell/src/lib/diligence-feature-shell.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { ShellComponent } from './shell/shell.component';
 import { SharedUiModule } from '@the-new-datasite/shared/ui';
 
 @NgModule({
-  imports: [CommonModule, SharedUiModule],
+  imports: [
+    CommonModule,
+    SharedUiModule,
+    RouterModule.forChild([
+      {
+        path: '',
+        component: ShellComponent,
+        children: [
+          {
+            path: 'content',
+            // component: ,
+            pathMatch: 'full'
+          }
+        ]
+      }
+    ]),],
   declarations: [ShellComponent],
   exports: [ShellComponent]
 })
