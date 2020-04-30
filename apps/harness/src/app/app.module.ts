3facf5d5c861152e4920008c20655eab24b3f1f7
nate knight <nateknight@gmail.com>
2020-04-30 15:39:09 -0500

    fix dependency issue

--- apps/harness/src/app/app.module.ts
+++ apps/harness/src/app/app.module.ts
@@ -3,12 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
+import { DocumentsFeatureShellModule } from '@the-new-datasite/documents/feature-shell';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([], { initialNavigation: 'enabled' })
+    DocumentsFeatureShellModule,
+    RouterModule.forRoot([], { initialNavigation: 'enabled' }),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- libs/diligence/feature-shell/src/lib/diligence-feature-shell.module.ts
+++ libs/diligence/feature-shell/src/lib/diligence-feature-shell.module.ts
@@ -3,15 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { ShellComponent } from './shell/shell.component';
 import { SharedUiModule } from '@the-new-datasite/shared/ui';
-// import { DocumentsShellComponent } from '@the-new-datasite/documents/documents-feature-shell';
-import { DocumentsShellComponent } from '../../../../documents/feature-shell/src/lib/documents-shell/documents-shell.component';
-// import { ShellComponent } from '@the-new-datasite/diligence/feature-shell';
+
 @NgModule({
   imports: [
     CommonModule,
     SharedUiModule,
-    RouterModule,
-    
+    RouterModule,    
   ],
   declarations: [ShellComponent],
   exports: [ShellComponent]
